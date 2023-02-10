4228d38fe438d9ecbdfa449371551c21b86ead89
subzero <shubham8111994@gmail.com>
2023-02-10 12:24:02 +0530

    Initial commit

--- src/com/addressbook/AddressBookMain.java
+++ src/com/addressbook/AddressBookMain.java
@@ -2,6 +2,6 @@ package com.addressbook;
 
 public class AddressBookMain {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome to Address Book Program");
     }
 }
--- src/com/addressbook/ContactPerson.java
+++ src/com/addressbook/ContactPerson.java
@@ -1,7 +1,5 @@
 package com.addressbook;
 
 public class ContactPerson {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Address Book Program");
-    }
+
 }
