8acb608e8475ed01e01d0ee4ec55f149dcc7dd7a
subzero <shubham8111994@gmail.com>
2023-02-10 14:12:27 +0530

    Added class contact details for person

--- src/com/addressbook/AddressBookMain.java
+++ src/com/addressbook/AddressBookMain.java
@@ -1,7 +1,34 @@
 package com.addressbook;
 
-public class AddressBookMain {
+import java.util.ArrayList;
+import java.util.Scanner;
+
+//Address book program
+public class AddressBookMain extends ContactPerson {
     public static void main(String[] args) {
+        AddressBookMain newPerson = new AddressBookMain();
+        ArrayList<AddressBookMain> person = new ArrayList<>();
         System.out.println("Welcome to Address Book Program");
+        Scanner scanner =new Scanner(System.in);
+        //Add person address
+        System.out.println("Enter first name");
+        newPerson.setFirstName(scanner.nextLine());
+        System.out.println("Enter last name");
+        newPerson.setLastName(scanner.nextLine());
+        System.out.println("Enter address");
+        newPerson.setAddress(scanner.nextLine());
+        System.out.println("Enter city");
+        newPerson.setCity(scanner.nextLine());
+        System.out.println("Enter state");
+        newPerson.setState(scanner.nextLine());
+        System.out.println("Enter zip code");
+        newPerson.setZip(scanner.nextInt());
+        System.out.println("Enter phone number");
+        newPerson.setPhoneNumber(scanner.nextInt());
+        System.out.println("Enter Email");
+        newPerson.setEmail(scanner.nextLine());
+        person.add(newPerson);
+
+        System.out.println(person);
     }
 }
--- src/com/addressbook/ContactPerson.java
+++ src/com/addressbook/ContactPerson.java
@@ -1,5 +1,96 @@
 package com.addressbook;
 
 public class ContactPerson {
+    String firstName = new String();
+    String lastName = new String();
+    String address = new String();
+    String city = new String();
+    String state = new String();
+    int zip;
+    long phoneNumber;
+    String email = new String();
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public ContactPerson(String firstName, String lastName, String address, String city, String state, int zip, int phoneNumber ){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.address=address;
+        this.city=city;
+        this.state=state;
+        this.zip=zip;
+        this.phoneNumber=phoneNumber;
+
+    }
+    public ContactPerson(){
+        super();
+    }
+    @Override
+    public String toString() {
+        return "Employee [name=" + this.firstName +" "+ this.lastName + ", Address=" + this.address + ", City="
+                + this.city + ", state=" + this.state + ", zip=" + this.zip
+                + ", phone=" + this.phoneNumber + "]";
+    }
 }
